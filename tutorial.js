3f0fcce11c074140853da299ce50e6906ab71ab2
tcjverburg <tom_verburg@hotmail.nl>
2015-09-30 22:38:34 +0200

    Create tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,122 @@
+
+
+/*
+// ############
+//    STEP 13
+// ############
+*/
+
+$('#link13').on('click', function(e){
+	
+	var query = $('#query13').val();
+	var endpoint = 'http://localhost:5820/week4/query';
+	var format = 'JSON';
+	
+	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
+		console.log(json);
+		
+		try {
+			var vars = json.head.vars;
+		
+			var ul = $('<ul></ul>');
+			ul.addClass('list-group');
+		
+			$.each(json.results.bindings, function(index,value){
+				var li = $('<li></li>');
+				li.addClass('list-group-item');
+			
+				$.each(vars, function(index, v){
+					var v_type = value[v]['type'];
+					var v_value = value[v]['value'];
+				
+					li.append('<strong>'+v+'</strong><br/>');
+				
+					// If the value is a URI, create a hyperlink
+					if (v_type == 'uri') {
+						var a = $('<a></a>');
+						a.attr('href',v_value);
+						a.text(v_value);
+						li.append(a);
+					// Else we're just showing the value.
+					} else {
+						li.append(v_value);
+					}
+					li.append('<br/>');
+					
+				});
+				ul.append(li);
+			
+			});
+			
+			$('#linktarget13').html(ul);
+		} catch(err) {
+			$('#linktarget13').html('Something went wrong!');
+		}
+		
+
+		
+	});
+	
+});
+
+
+/*
+// ############
+//    STEP 14
+// ############
+*/
+
+$('#link14').on('click', function(e){
+	
+	var query = $('#query14').val();
+	var endpoint = 'http://localhost:5820/week4/query';
+	var format = 'JSON';
+	
+	$.get('/sparql2',data={'endpoint': endpoint, 'query': query, 'format': format}, function(json){
+		console.log(json);
+		
+		try {
+			var vars = json.head.vars;
+		
+			var ul = $('<ul></ul>');
+			ul.addClass('list-group');
+		
+			$.each(json.results.bindings, function(index,value){
+				var li = $('<li></li>');
+				li.addClass('list-group-item');
+			
+				$.each(vars, function(index, v){
+					var v_type = value[v]['type'];
+					var v_value = value[v]['value'];
+				
+					li.append('<strong>'+v+'</strong><br/>');
+				
+					// If the value is a URI, create a hyperlink
+					if (v_type == 'uri') {
+						var a = $('<a></a>');
+						a.attr('href',v_value);
+						a.text(v_value);
+						li.append(a);
+					// Else we're just showing the value.
+					} else {
+						li.append(v_value);
+					}
+					li.append('<br/>');
+					
+				});
+				ul.append(li);
+			
+			});
+			
+			$('#linktarget14').html(ul);
+		} catch(err) {
+			$('#linktarget14').html('Something went wrong!');
+		}
+		
+
+		
+	});
+	
+});
+
+
